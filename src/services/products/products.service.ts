3abdc13d54d54a0eef2516fb11e903d26e96e05b
Mahmoudhamdy00 <mahmoudh.morsy@gmail.com>
2024-05-18 00:20:20 +0300

    products service

--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../../models/Prodoct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  DB_URL = 'https://fakestoreapi.com/products';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  GetAllProducts() {
+    return this.http.get(this.DB_URL);
+  }
+  GetProductById(id: number) {
+    return this.http.get(this.DB_URL + "/" + id);
+  }
+  AddProduct(product: Product) {
+    return this.http.post(this.DB_URL, product);
+  }
 }
