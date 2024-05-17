a4ad36dbd7fe2a9924e6e586f2413c8d6a061f86
Mahmoudhamdy00 <mahmoudh.morsy@gmail.com>
2024-05-18 00:29:56 +0300

    users service

--- src/services/users/user.service.ts
+++ src/services/users/user.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  DB_URL = 'http://localhost:3000/users';
+  constructor(private http: HttpClient) { }
+
+  GetAllUsers() {
+    return this.http.get(this.DB_URL);
+  }
+  GetUserById(id: number) {
+    return this.http.get(this.DB_URL + "/" + id);
+  }
+  AddUser(user: User) {
+    return this.http.post(this.DB_URL, user);
+  }
 }
