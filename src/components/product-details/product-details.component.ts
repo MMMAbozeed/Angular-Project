05adec3c6a4f2565ece59bacb21703de34875e03
Abdelrahman-Samir12 <abdelrahmansamir326@gmail.com>
2024-05-19 14:07:33 +0300

    ProductDetailsAdded

--- src/components/product-details/product-details.component.ts
+++ src/components/product-details/product-details.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommentsService } from '../../services/comments/comments.service';
+import { Comment } from '../../models/Comment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,CommonModule],
+  providers: [CommentsService],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
-export class ProductDetailsComponent {
-
+export class ProductDetailsComponent implements OnInit {
+  @Input() product: any;
+  constructor(private commentsService: CommentsService) {
+    console.log('data');
+  }
+  list:any[]|undefined;
+  ngOnInit(): void {
+    console.log('sssss',this.commentsService.GetProductComments(2).subscribe(
+      (data: any) => {
+        this.list = data;
+        console.log('ssssssddd',data);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    ));
+    
+  }
+  haha(){
+    console.log('dd',this.list);
+  }
 }
