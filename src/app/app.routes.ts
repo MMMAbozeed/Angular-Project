1360ba7a2659f4f8b2340f160ac8912314347fdc
MMMAbozeed <mmfahimejada@gmail.com>
2024-05-19 13:56:22 +0300

    v3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
+import { ProductDetailsComponent } from '../components/product-details/product-details.component';
+import { LoginComponent } from '../components/login/login.component';
+import { SignUpComponent } from '../components/sign-up/sign-up.component';
 
 export const routes: Routes = [
-  {path: '', component:HomeComponent}
+  {path: '', component:HomeComponent},
+  {path:'product-detail', component:ProductDetailsComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignUpComponent},
 ];
